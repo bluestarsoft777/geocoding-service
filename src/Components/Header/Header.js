7c72cdde61d76a5dba9392fb6bf77a74feb76c73
kgmsoft <princehome0505@gmail.com>
2017-05-23 21:56:50 +0200

    add link to an example file with locations

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './style.css';
 
+const exampleString = `
+[{
+    street: '350 Fifth Avenue',
+    city: 'New York',
+    country: 'USA'
+}, ...]
+`;
+
 const Header = () => {
     return (
         <header className="header">
-            <h1><span>Geocoding</span> <span>service</span></h1>
+            <h1 className="header__title">
+                <span>Geocoding</span> <span>service</span>
+            </h1>
+            <div className="header__info">
+                To use upload a JSON file containing locations formatted like the
+                {' '}
+                <a
+                    href="/locations_example.json"
+                    download
+                    className="header__info-link"
+                >
+                    following
+                </a>
+            </div>
         </header>
     );
 };
